03b1f349d24b61e5b19ecc687504677709b28948
Miguel Rochefort <miguelrochefort@gmail.com>
2020-05-11 09:39:20 -0400

    Add NotificationWatcher

---
+++ mobile/src/main/java/net/activitywatch/android/watcher/NotificationWatcher.kt
@@ -0,0 +1,56 @@
+package net.activitywatch.android.watcher
+
+import android.app.Notification
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.session.MediaController
+import android.media.session.MediaSessionManager
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+import net.activitywatch.android.RustInterface
+import org.json.JSONObject
+import org.threeten.bp.Instant
+
+class NotificationWatcher : NotificationListenerService() {
+    private val TAG = this.javaClass.simpleName
+    private val bucket_id = "aw-watcher-android-notifications"
+
+    private var ri : RustInterface? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        ri = RustInterface(applicationContext)
+        ri?.createBucketHelper(bucket_id, "notification") // TODO: Change event type
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ri = null
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        onNotification(sbn)
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        onNotification(sbn)
+    }
+
+    fun onNotification(sbn: StatusBarNotification) {
+        val notification = sbn.notification;
+        val title = notification.extras.get(Notification.EXTRA_TITLE);
+        val text = notification.extras.get(Notification.EXTRA_TEXT)
+        if (title != null) {
+            val timestamp = Instant.ofEpochMilli(System.currentTimeMillis())
+            val duration = 1.0 // TODO
+            val data = JSONObject()
+            data.put("package", sbn.packageName)
+            data.put("title", title)
+            data.put("text", text)
+            ri?.heartbeatHelper(bucket_id, timestamp, duration, data)
+        }
+    }
+}
